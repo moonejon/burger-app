ff48312d56193458174cc8ec6f30c23f817365d1
Jonathan Mooney <moonejon@gmail.com>
2019-03-14 00:07:23 -0500

    adding boilerplate and setting up database

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+use burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty Melt', true);
