e5a38fa915827e61360534f87a970cb0818a90e9
Jonathan Mooney <moonejon@gmail.com>
2019-03-15 19:59:44 -0500

    added models

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.all("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    createOne: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
